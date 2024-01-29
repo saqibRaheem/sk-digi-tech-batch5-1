69127351b9017cab4d1094bc66b2a199139635fb
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-01-29 19:03:59 +0500

    add class javascript

---
+++ javascript/class 2/app.js
@@ -0,0 +1,120 @@
+// var a = prompt("Enter your name")
+// if(a === "awais"){
+//     alert("welcome awais")
+// }else{
+//     alert("Not")
+// }
+
+
+
+// if(a === "awais"){
+// alert("Welcome awais")
+// }
+// 5 <= 5
+// 6 > 5
+// 6 >= 6
+
+
+// var a = 5;
+// var b = 4;
+// if (a > b){
+//     alert("Condition is true")
+// }
+
+
+// alert("hello world!")
+
+
+// var myname;
+// myname = 12231231
+
+// var weight = 150
+
+// weight =  weight + 50
+
+
+
+// var myNmae = "saqib";
+
+// alert(myNmae)
+
+
+
+
+// POST INCREMENT
+
+
+
+// var num = 5;
+//   num++  
+//   num--
+
+//   alert(num)
+
+
+
+// PRE INCREMENT
+
+
+// var num = 5
+
+// ++num
+//  --num
+// alert(num)
+
+
+// var num = 5;
+// var num2 = num++ + ++num + num++ - num
+    //      5  + 7 + 7 - 8
+// alert(num)
+// alert(num2)
+
+
+
+
+// var num = +prompt("enter your first number")
+// var num2 = +prompt("enter your second number")
+
+// var result = num + num2
+// alert(result)
+
+
+// var age = prompt("entery your age")
+
+
+// if(age >= 18){
+//     alert("you are eligable for vote ")
+// }else{
+//     alert("you are not eligable for vote ")
+// }
+
+ var city = prompt("enter your city");
+
+ if(city === "karachi"){
+    alert("city of light")
+}else if(city === "lahore"){
+    
+    alert(" beautiful city of ")
+ }else if(city === "islamabad"){
+    
+    alert(" cleanest city of ")
+ }else{
+    
+    alert(" not found ")
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
