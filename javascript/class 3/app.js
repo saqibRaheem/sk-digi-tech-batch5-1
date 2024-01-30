36051c2b8aec0ed6edb8a62e19698020a208b551
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-01-30 12:29:53 +0500

    add js class 3
    condition statement

---
+++ javascript/class 3/app.js
@@ -0,0 +1,67 @@
+
+
+
+// var age = prompt("enter your age")
+// var gender = prompt("enter your gender")
+
+// if(age > 18 && gender === "male" || gender === "female" ){
+//    alert("you are eligable")
+// }else{
+//     alert("you are not eligable")
+// }
+
+// var a = 5;
+// var b = "5";
+// var c = 9;
+//  var d = 6;
+
+// if( a === b){
+
+//     if(c === d){
+//         alert("Match")
+//     }else{
+//         alert("match but not found")
+//     }
+
+
+// }else{
+//     alert("not match")
+// }
+
+
+// var range = prompt("enter your range")
+// var brand = prompt("enter your brand name")
+
+
+// if(range > 45000 && brand === "hp"){
+
+//     alert("HP laptop available")
+
+// }else if((range == 20000 || range <= 35000) && brand === "dell"){
+
+//     alert("Dell laptop available")
+
+// }else{
+
+//     alert("laptop not available")
+// }
+
+var city3 = "islamabad"
+var cityes = [ "karachi", "lahore","islamabad",23482734,20394234,true,false ]
+
+
+// alert("wellcome " + city3);
+// document.write("wellcome");
+console.log("wellcome " + cityes[6]);
+
+
+
+
+
+
+
+
+
+
+
+
