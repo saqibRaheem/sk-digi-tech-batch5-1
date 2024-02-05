d64f1c4990e889f198b329f049dfe5b7f8c1aff6
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-02-05 14:27:53 +0500

    add class

---
+++ javascript/class 4/app.js
@@ -0,0 +1,48 @@
+// var des = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ----”"
+
+// // alert(des);
+
+// var num =  4;
+// document.write(num + " * 1 = " + num , "<br/>")
+// document.write(num + " * 2 = " + num * 2)
+
+
+// var saqib = [1,2,3,4,5,6,7,8,9,0, "data1","data2",true,false]
+// console.log(saqib);
+// document.write(saqib)
+
+// var city0 = "karachi "
+// var city1 = "karachi 1"
+// var city2 = "karachi 2"
+// var city3 = "karachi 3"
+ 
+// var citys = []
+
+// citys[0] = " punjab "
+// citys[1] = " lahore " 
+// citys[4] = " karachi" 
+
+// console.log("initial array ",citys);
+// citys.pop()
+// citys.pop()
+// citys.pop()
+// console.log("pop method ",citys);
+// citys.push("karachi","larkana")
+// console.log("add push method",citys);
+// citys.shift()
+// console.log("use method shift",citys);
+// citys.unshift("punjab")
+// console.log("use method unshift",citys);
+
+// console.log("third ",citys);
+// console.log(citys[4]);
+
+
+var birds = [ "dog", "cat", "fly", "bug", "ox"]
+console.log( "initial array ",birds);
+
+// var copy = birds.slice(2,2)
+// console.log(copy);
+
+birds.splice(2,1,"pig", "duck", "emu")
+console.log("after removing elements and adds",birds);
