672acec07ae4b0d1ea2f62544022031be6681260
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-04-28 15:53:58 +0500

    add class 12

---
+++ javascript/class 12/app.js
@@ -0,0 +1,45 @@
+// var text = "bilal12";
+
+// if(text == "saqib"){
+//     console.log("text is matched");
+// }else if(text == "bilal"){
+//     console.log("name is matched " + text);
+// }
+
+// switch(text){
+//     case 'saqib':  alert("name is matched");
+//    break;
+//     case "bilal": alert("name is match bilal")
+//    break;
+//    case "rauf":alert("name is match rauf")
+
+// break;
+// default:alert("words not match")
+// }
+
+// for(i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// var j = 11
+// while(j <10){
+//     console.log(">>>",j);
+// j++
+// }
+
+
+// var k = 0  
+// do{
+//     console.log("fo do while",k);
+//     k++
+// }while (k <=10); 
+
+
+function add(num1,num2){
+   
+    alert(num1+num2)
+}
+
+function clickBtn(){
+    console.log("click");
+}
